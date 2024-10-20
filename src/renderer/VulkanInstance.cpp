ed513c3fba04c94a9297d54807946c1e328ff439
bgrhnzcn <bugrahanozcan16@gmail.com>
2024-10-21 00:36:37 +0300

    Refactor VulkanInstance class to include debug messenger and select physical device

--- src/renderer/VulkanInstance.cpp
+++ src/renderer/VulkanInstance.cpp
@@ -6,7 +6,7 @@
 /*   By: bgrhnzcn <bgrhnzcn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 03:45:21 by bgrhnzcn          #+#    #+#             */
-/*   Updated: 2024/10/20 04:03:49 by bgrhnzcn         ###   ########.fr       */
+/*   Updated: 2024/10/21 00:01:09 by bgrhnzcn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,28 @@ VulkanInstance::VulkanInstance()
 	//We get how many extansion used by glfw and what is their names.
 	//We will use this data to make Vulkan to load this extansions.
 	glfwExtNames = glfwGetRequiredInstanceExtensions(&glfwExtCount);
+	std::vector<const char *> extensions(glfwExtNames, glfwExtNames + glfwExtCount);
+	extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+	for (const char *val : extensions)
+		std::cout << val << std::endl;
 	//Not mandotory, useful to make driver work better.
 	VkApplicationInfo appInfo = InitAppInfo();
+	//Variables to store data about Validation layers.
+	uint32_t layerCount = 0;
+	std::vector<VkLayerProperties> availableLayers;
+	std::vector<const char *> layers;
+	if (enableDebug)
+		//We are adding requested layers.
+		layers.push_back("VK_LAYER_KHRONOS_validation");
+	//Not mandotory. Checks existence of validation layers. If fail program aborts.
+	GetValidationLayers(layerCount, availableLayers, layers);
 	//Mandatory. We use this data to initialize Vulkan.
 	//Every entry in here used for some important aspect of Vulkan Instance.
-	VkInstanceCreateInfo createInfo = InitCreateInfo(appInfo, glfwExtCount, glfwExtNames);
-	//Variables to store data about Validation layers.
-	uint32_t layerCount;
-	std::vector<VkLayerProperties> availableLayers;
-	std::vector<const char *> layerRequests;
-	//We are adding requested layers.
-	layerRequests.push_back("VK_LAYER_KHRONOS_validation");
-	//Not mandotory. Checks existence of validation layers. If fail program aborts.
-	GetValidationLayers(layerCount, availableLayers, layerRequests);
+	VkInstanceCreateInfo createInfo = InitCreateInfo(appInfo, extensions, layers);
 	//Finally create our Vulkan Instance. (Similiar to OpenGL Context)
 	vkCreateInstance(&createInfo, nullptr, &vkInst);
+	CreateDebugMessanger();
+	SelectPhysicalDevice();
 }
 
 VulkanInstance::~VulkanInstance()
@@ -56,8 +63,47 @@ VkApplicationInfo VulkanInstance::InitAppInfo()
 	};
 }
 
-VkInstanceCreateInfo VulkanInstance::InitCreateInfo(const VkApplicationInfo &appInfo,
-	const uint32_t glfwExtCount, const char **glfwExtNames)
+VkDebugUtilsMessengerCreateInfoEXT VulkanInstance::InitDebugMessenger(
+)
+{
+	return VkDebugUtilsMessengerCreateInfoEXT
+	{
+		.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
+		.pNext = nullptr,
+		.flags = {},
+		.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT
+						|  VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT
+						|  VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT,
+		.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT,
+		.pfnUserCallback = VulkanInstance::DebugCallback,
+		.pUserData = nullptr
+	};
+}
+
+void VulkanInstance::CreateDebugMessanger()
+{
+	//This part creates debug messenger for callbacks and validation layers.
+	//We will load this function manually because it is part of an extension.
+	auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(vkInst, "vkCreateDebugUtilsMessengerEXT");
+	if (func == nullptr)
+		throw std::runtime_error("Failed to load extension function: vkCreateDebugUtilsMessengerEXT");
+	VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo = InitDebugMessenger();
+	func(vkInst, &debugCreateInfo, nullptr, &debugMessenger);
+}
+
+void VulkanInstance::SelectPhysicalDevice()
+{
+	uint32_t deviceCount = 0;
+	vkEnumeratePhysicalDevices(vkInst, &deviceCount, nullptr);
+	std::vector<VkPhysicalDevice> devices(deviceCount);
+	vkEnumeratePhysicalDevices(vkInst, &deviceCount, devices.data());
+	device = devices[0];
+}
+
+VkInstanceCreateInfo VulkanInstance::InitCreateInfo(
+	const VkApplicationInfo &appInfo,
+	std::vector<const char *> &extensions,
+	std::vector<const char *> &layers)
 {
 	return VkInstanceCreateInfo
 	{
@@ -65,14 +111,44 @@ VkInstanceCreateInfo VulkanInstance::InitCreateInfo(const VkApplicationInfo &app
 		.pNext = nullptr,
 		.flags = {},
 		.pApplicationInfo = &appInfo,
-		.enabledLayerCount = 0,
-		.ppEnabledLayerNames = nullptr,
-		.enabledExtensionCount = glfwExtCount,
-		.ppEnabledExtensionNames = glfwExtNames
+		.enabledLayerCount = static_cast<uint32_t>(layers.size()),
+		.ppEnabledLayerNames = layers.data(),
+		.enabledExtensionCount = static_cast<uint32_t>(extensions.size()),
+		.ppEnabledExtensionNames = extensions.data()
 	};
 }
 
-void VulkanInstance::GetValidationLayers(uint32_t &layerCountOut, std::vector<VkLayerProperties> &availableLayers, std::vector<const char *> &layerRequests)
+VKAPI_ATTR VkBool32 VKAPI_CALL VulkanInstance::DebugCallback(
+	VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+	VkDebugUtilsMessageTypeFlagsEXT messageType,
+	const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+	void* pUserData)
+{
+	(void)pUserData;
+	if (messageType != VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT)
+		return VK_FALSE;
+	switch (messageSeverity)
+	{
+		case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:
+			std::cout << "[ INFO ] " << pCallbackData->pMessage << std::endl;
+			break;
+		case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
+			std::cout << "[ WARNING ] " << pCallbackData->pMessage << std::endl;
+			break;
+		case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
+			std::cout << "[ ERROR ] " << pCallbackData->pMessage << std::endl;
+			break;
+		default:
+			std::cout << "[ OTHER ] " << pCallbackData->pMessage << std::endl;
+			break;
+	}
+	return VK_FALSE;
+}
+
+void VulkanInstance::GetValidationLayers(
+	uint32_t &layerCountOut,
+	std::vector<VkLayerProperties> &availableLayers,
+	std::vector<const char *> &layerRequests)
 {
 	uint32_t layerCount;
 	vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
@@ -102,3 +178,4 @@ void VulkanInstance::GetValidationLayers(uint32_t &layerCountOut, std::vector<Vk
 	}
 	std::cout << "Requested Validation layers Available!" << std::endl;
 }
+
--- src/renderer/VulkanInstance.hpp
+++ src/renderer/VulkanInstance.hpp
@@ -6,12 +6,18 @@
 /*   By: bgrhnzcn <bgrhnzcn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 03:20:58 by bgrhnzcn          #+#    #+#             */
-/*   Updated: 2024/10/20 04:03:58 by bgrhnzcn         ###   ########.fr       */
+/*   Updated: 2024/10/20 22:48:57 by bgrhnzcn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VULKAN_INSTANCE_HPP
 #	define VULKAN_INSTANCE_HPP
+
+#	ifndef NDEBUG
+	const static bool enableDebug = false;
+#	else
+	const static bool enableDebug = true;
+#	endif
 
 #	define GLFW_INCLUDE_VULKAN
 #	include <glfw3.h>
@@ -25,13 +31,26 @@ class VulkanInstance
 		~VulkanInstance();
 	private:
 		VkInstance vkInst;
+		VkDebugUtilsMessengerEXT debugMessenger;
+		VkPhysicalDevice device;
 	private:
 		VkApplicationInfo InitAppInfo();
 		VkInstanceCreateInfo InitCreateInfo(const VkApplicationInfo &appInfo,
-				const uint32_t glfwExtCount, const char **glfwExtNames);
+				std::vector<const char *> &extensions,
+				std::vector<const char *> &layers);
 		void GetValidationLayers(uint32_t &layerCountOut,
 				std::vector<VkLayerProperties> &availableLayers,
 				std::vector<const char *> &layerRequests);
+		VkDebugUtilsMessengerCreateInfoEXT InitDebugMessenger(
+				);
+		void CreateDebugMessanger();
+		void SelectPhysicalDevice();
+	private: //Static Members
+		static VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallback(
+				VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+				VkDebugUtilsMessageTypeFlagsEXT messageType,
+				const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+				void* pUserData);
 };
 
 #endif //VULKAN_INSTANCE_HPP
